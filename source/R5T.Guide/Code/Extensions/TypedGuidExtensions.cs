362c5132a819c0bba7d54dca2bd4241009ad7b91
David Coats <david@getsafetycone.com>
2021-03-28 22:05:11 -0700

    Add extension method to get distinct values of values, and to convert an enumerable of values to an enumerable of a typed-Guid

--- source/R5T.Guide/Code/Extensions/TypedGuidExtensions.cs
+++ source/R5T.Guide/Code/Extensions/TypedGuidExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using R5T.Guide;
 
 
+// In the System namespace to allow referencing the R5T.Guide library to be enough indication-of-interest in the extension methods (since by habit System is always used in code files).
 namespace System
 {
     /// <summary>
@@ -10,10 +13,23 @@ namespace System
     /// </summary>
     public static class TypedGuidExtensions
     {
+        public static IEnumerable<Guid> DistinctValues<TTypedGuid>(this IEnumerable<TTypedGuid> typedGuids)
+            where TTypedGuid : TypedGuid
+        {
+            var output = typedGuids.Select(x => x.Value).Distinct();
+            return output;
+        }
+
         public static bool IsEmpty(this TypedGuid typedGuid)
         {
             var isEmpty = typedGuid.Value == Guid.Empty;
             return isEmpty;
+        }
+
+        public static IEnumerable<TTypedGuid> ToType<TTypedGuid>(this IEnumerable<Guid> guids, Func<Guid, TTypedGuid> constructor)
+        {
+            var output = guids.Select(x => constructor(x));
+            return output;
         }
     }
 }
